af895a00211525dcfea762962d1ae27096f685f1
bellalhrbubt <bellalhrbubt@gmail.com>
2019-11-17 21:16:13 +0600

    Add app screenshort

--- lib/main.dart
+++ lib/main.dart
@@ -19,20 +19,6 @@ class _BmiAppState extends State<BmiApp> {
   void onTabTapped(int index) {
     setState(() {
       _selectedIndex = index;
-
-      /* Fluttertoast.showToast(
-          msg: "This is Center Short Toast" + _selectedIndex.toString(),
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.BOTTOM,
-          timeInSecForIos: 1,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);*/
-
-      /* Navigator.of(context)
-          .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-        return new NewPage();
-      }));*/
     });
   }
 
